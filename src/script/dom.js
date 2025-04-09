9a58076fc86b366ddfbc5b0a4191c2ed0bd69022
ivan <ivanmangulabnan19@gmail.com>
2025-04-09 17:07:30 +0800

    Add dom script for dom nodes

---
+++ src/script/dom.js
@@ -0,0 +1,8 @@
+export class Header {
+  static get dropdownDiv() {
+    return document.querySelector('.dropdown-div');
+  }
+  static get dropdownIcon() {
+    return document.querySelector('.dropdown-icon');
+  }
+}
