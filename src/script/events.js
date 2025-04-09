8400837c440e362d91bec4c2d8da4f14033cb791
ivan <ivanmangulabnan19@gmail.com>
2025-04-09 17:07:58 +0800

    Moved util functions

--- src/script/events.js
+++ src/script/events.js
@@ -1,14 +1,1 @@
 export class Events { }
-
-export function dropdownEvent() {
-  const dropdownButton = document.querySelector('.dropdown-icon');
-  const dropdownDiv = document.querySelector('.dropdown-div');
-
-  dropdownButton.addEventListener('mouseover', event => {
-    dropdownDiv.classList.toggle('visible');
-  })
-
-  dropdownDiv.addEventListener('mouseleave', event => {
-    dropdownDiv.classList.toggle('visible');
-  })
-}
