72ba8b6f485a0f26c0886a63c9197f7d56072da8
ivan <ivanmangulabnan19@gmail.com>
2025-04-11 11:35:02 +0800

    Add event listeners for click right and left button

--- src/script/events.js
+++ src/script/events.js
@@ -1,1 +1,16 @@
-export class Events { }
+import { Main } from "./dom.js";
+import { moveDisplayedImage } from "./util.js";
+
+export class Events { 
+  static click () {
+    document.addEventListener('click', event => {
+      if (event.target === Main.btnRight){
+        moveDisplayedImage();
+      }
+
+      if (event.target === Main.btnLeft){
+        moveDisplayedImage(Main.btnLeft);
+      }
+    })
+  }
+}
