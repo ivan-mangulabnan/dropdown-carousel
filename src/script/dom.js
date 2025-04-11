376d645823fea9f5d2417bac41ac75047742d68c
ivan <ivanmangulabnan19@gmail.com>
2025-04-11 11:38:02 +0800

    Add dom elements

--- src/script/dom.js
+++ src/script/dom.js
@@ -1,8 +1,38 @@
 export class Header {
-  static get dropdownDiv() {
+  static get dropdownDiv () {
     return document.querySelector('.dropdown-div');
   }
-  static get dropdownIcon() {
+  static get dropdownIcon () {
     return document.querySelector('.dropdown-icon');
+  }
+
+  static get header () {
+    return document.querySelector('header');
+  }
+}
+
+export class Main {
+  static get main () {
+    return document.querySelector('main');
+  }
+
+  static get miniBtnContainer () {
+    return document.querySelector('.mini-btn-container');
+  }
+
+  static get imageContainer () {
+    return document.querySelector('.img-container');
+  }
+
+  static get btnRight () {
+    return document.querySelector('.right');
+  }
+
+  static get btnLeft () {
+    return document.querySelector('.left');
+  }
+
+  static get miniBtnContainer () {
+    return document.querySelector('.mini-btn-container');
   }
 }
