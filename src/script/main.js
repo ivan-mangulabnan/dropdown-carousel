4062e8e2a9de4fab3769acf468fc3d45fd29d88e
ivan <ivanmangulabnan19@gmail.com>
2025-04-11 11:27:23 +0800

    Add IIFE that sets a glow to buttons corresponding to the index of its image counterpart

--- src/script/main.js
+++ src/script/main.js
@@ -8,3 +8,11 @@ import { Main } from "./dom.js";
     Main.miniBtnContainer.appendChild(miniBtn);
   })
 })();
+
+(function () { // Applies a red glow to the mini-button with the same location of counterpart image.
+  const images = Array.from(Main.imageContainer.querySelectorAll('div'));
+  const miniBtns = Array.from(Main.miniBtnContainer.querySelectorAll('button'));
+  const focus = images.findIndex(img => img.classList.contains('img'));
+
+  miniBtns[focus].classList.add('focus-btn');
+})();
